a948e0867a6643cca51990cf1a14a7f9187b1110
Kai Hicken <khckn1@gmail.com>
2022-10-24 08:41:43 -0600

    most functions defined

--- pseudocode.py
+++ pseudocode.py
@@ -1,5 +1,5 @@
 
-
+import AltAzRange
   # gps installation
 #figure out how to get lat, long, elev from the gps - maybe can be manually loaded on installation
 
@@ -8,8 +8,8 @@
 lat_pr = 40.246638
 long_pr = -111.646856
 alt_pr = 1439 #meters
-#az_ant = ?  
-#el_ant = ?
+az_ant = 0
+el_ant = 0
 
 # acquisition
   # user input
@@ -25,28 +25,44 @@ def ui(satellite, antenna, threshold):
   # output: signal strength, current, heading, connected satellite
   # continuous while moving, update 1/loop
   # GUI - what library to make using?
+  maintain()
 
 
-def getLat(satellite):
+def getLat(TLE):
   """Uses TLE to get latitude of the satellite"""
 
-def getLong(satellite):
+def getLong(TLE):
   """Uses TLE to get longitude of the satellite"""
 
-def acquire(satellite, antenna, threshold):
+def acquire(TLE, antenna, threshold):
   """acquires and focuses in on new antenna"""
-  latSat = getLat(satellite)
-  longSat = getLong(satellite)
-# Satellite to acquire
-  # satellite lat, long, elev(35,786 km), polarization
-  # find from a database somewhere??
-  # TLE????
+  latSat = getLat(TLE)
+  longSat = getLong(TLE)
+  # Satellite to acquire
+    # satellite lat, long, elev(35,786 km), polarization
+    # find from a database somewhere??
+    # TLE????
+  orient = getNewOrientation(latSat,longSat)
+
+  move(az_ant - orient["azimuth"], el_ant - orient["elevation"])
+  polarization()
+  focus()
+
 
 def orient():
   """orients itself, saves as global variables"""
+  global az_ant, el_ant
+  az_ant = 0#antenna azimuth
+  el_ant = 0#antenna elevation
 
-# calculate new az/el
-    # math
+def getNewOrientation(latSat,longSat):
+  """uses AltAzRange to calculate the new orientation for the antenna"""
+  satellite = AltAzRange.AltAzimuthRange()
+  satellite.observer(lat_pr,long_pr,alt_pr)
+  satellite.target(latSat, longSat, 35786000)
+  # calculate new az/el
+      # math
+  return satellite.calculate()#orient is a dictionary
 
 # desired db threshold
   # also from a database??
@@ -59,26 +75,46 @@ def orient():
   # count number of pulses to figure how far it has moved
     # convert that to degrees moved/ elevaion change
 
+def move(az, el):
+  """moves actuators to the right place - only rough moving"""
 # move az/el
   # compass data, hall sensors - inputs
   # move until compass/hall sensors are correct location
+  moveAz(az)
+  moveEl(el)
+
+def moveAz(az):
+  """moves slew drive"""
+
+def moveEl(el):
+  """moves linear actuator"""
   
+def focus():
+  """focuses antenna on the satellite"""
 # focus antenna using db threshold
+
+def polarization():
+  """matches polarization to the satellite"""
   # something using polarization which we dont understand???
   # do something like 30% 70% on the polarization sensors
 
+def maintain():
+  """maintains gain by constantly focusing in on peak or threshold, awaits further commands"""
+  # maintain gain
+    # check every x hr?/min? to see if gain has changed
+      # is it caused by atmospheric interference or drift?
+        # check weather ??? is this necessary????
+    # change if known gain loss is drift
 
+    #maybe repeat focus()
+  focus()
+    
+    # repeat finding peak gain (if we dont care about other sources of loss)
 
-# maintain gain
-  # check every x hr?/min? to see if gain has changed
-    # is it caused by atmospheric interference or drift?
-      # check weather ??? is this necessary????
-  # change if known gain loss is drift
-  
-  # repeat finding peak gain (if we dont care about other sources of loss)
-
-# await further commands
-  # sm state
+  # await further commands
+    # sm state
+  if (furtherCommand):
+    ui(TLE, antenna, dB)
   
   
   # possible sm setup
