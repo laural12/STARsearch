1704141145d2c6b3e3f80c2ce4a6b133617495d4
laural12 <laura.m.landon@gmail.com>
2022-10-24 09:32:40 -0600

    add comments

--- altAzCalc.py
+++ altAzCalc.py
@@ -6,7 +6,9 @@
 from AltAzRange import AltAzimuthRange
 
 satellite = AltAzimuthRange()
-satellite.observer(46.947, 7.4442, 1387)
-satellite.target(0, 13, 35800000)
+satellite.observer(46.947, 7.4442, 1387)  # Params: lat, lon, altitude
+satellite.target(0, 13, 35800000)  # Params: lat, lon, altitude
 
 print(satellite.calculate())
+
+# Outputs: {'azimuth': 172.39, 'elevation': 35.78, 'distance': 38121332.3}
