3226684698def507d8f5cbe2566a92d27073402d
birdo3000 <95247478+birdo3000@users.noreply.github.com>
2023-01-25 08:55:06 -0700

    Update orientation.py

--- orientation.py
+++ orientation.py
@@ -14,5 +14,13 @@ def orientation_init():
   while (not elLimitswitchHit())
     time.sleep(1)
   rot.elReset()
-   
   
+  #set orientation to default orientation
+  
+def orientToPoint(antLat, antLong, antElevation, satLat, satLong)
+  #math equation
+   
+  #if az angle 0-180 turn right
+  #else 181-360 turn left or smth like that
+  
+  rot.elTurnUp() #until reaches correct angle
