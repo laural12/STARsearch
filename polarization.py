1a9c20ad3d581bce986b2010c25236ed1e0af3cd
birdo3000 <95247478+birdo3000@users.noreply.github.com>
2023-01-27 08:46:06 -0700

    Update polarization.py

--- polarization.py
+++ polarization.py
@@ -4,8 +4,14 @@ class Polarization:
   def __init__(self):
     #?
   
-  def readPol:
-    #?
+  def readPol():
+    # returns the gain from the feed
     
-  def findPeakPol:
-    #?
+  def orientPol():
+    # rotate feed to angle of polarization? is this angle known?
+    
+  def findPeakPol():
+    # moves up/down left/right till polarization reaches max
+    
+  def maintainPeakPol():
+    # if gain decreases, rerun findPeakPol
