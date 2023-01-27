503d975c3446addcfc43213ca7c1234749a9a922
birdo3000 <95247478+birdo3000@users.noreply.github.com>
2023-01-27 08:18:11 -0700

    Update orientation.py

--- orientation.py
+++ orientation.py
@@ -8,31 +8,24 @@ class Orientation:
     
     def __init__(self):
       rot = Rotation(GUI_test = False)
-
       self.AZ_LIMIT_ORIENTATION = 200 #this is the orientation at the limit switch
       self.EL_LIMIT_ORIENTATION = 0 #this is the orientation at the limit switch
 
-    def orientation_init():
-      
-      
+    def orientation_init():   
       wpi.wiringPiISR(LIMIT_ENC_AZ, wpi.INT_EDGE_BOTH, self.azLimitswitchHit)
       wpi.wiringPiISR(LIMIT_ENC_EL, wpi.INT_EDGE_BOTH, self.elLimitswitchHit)
-
       rot.azTurnLeft()
       rot.elTurnDown()
 
     def orientToPoint(antLat, antLong, antElevation, satLat, satLong):
       #math equation
-
       #if az angle 0-180 turn right
       #else 181-360 turn left or smth like that
-
       rot.elTurnUp() #until reaches correct angle
 
     def azLimitswitchHit():
       rot.azReset()
       Orientation.az = AZ_LIMIT_ORIENTATION
-
 
     def elLimitswitchHit():
       rot.elReset()
