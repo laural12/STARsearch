d9eae44a8c9a14e173262cf70d69945235e26bca
Emily (odroid) <emily@smarts.us>
2023-02-01 16:27:11 +0000

    *debounce* limit switch kinda

--- orientation.py
+++ orientation.py
@@ -14,16 +14,28 @@ class Orientation:
     el = 0
     
     def __init__(self):
+      # init variables
       self.rot = Rotation(GUI_test = False)
       self.AZ_LIMIT_ORIENTATION = 200 #this is the orientation at the limit switch
       self.EL_LIMIT_ORIENTATION = 0 #this is the orientation at the limit switch
+      self.DEBOUNCE_VAL = 3 # Arbitrary; how many triggers to wait before trusting the limit switch
+      
+      self.azLimTriggers = 0
+      self.elLimTriggers = 0
+      self.azOrientDone = True
+      self.elOrientDone = True
+      
+      # Function setup
+      wpi.wiringPiISR(self.rot.LIMIT_ENC_AZ, wpi.INT_EDGE_RISING, self.azLimitswitchHit)
+      # wpi.wiringPiISR(self.rot.LIMIT_ENC_EL, wpi.INT_EDGE_RISING, self.elLimitswitchHit)
 
     def orientation_init(self):   
       print("orientation_init")
-      wpi.wiringPiISR(self.rot.LIMIT_ENC_AZ, wpi.INT_EDGE_BOTH, self.azLimitswitchHit)
-      # wpi.wiringPiISR(self.rot.LIMIT_ENC_EL, wpi.INT_EDGE_BOTH, self.elLimitswitchHit)
-      # rot.azTurnLeft()
-      # rot.elTurnDown()
+      self.azOrientDone = False
+      self.elOrientDone = False
+      
+      rot.azTurnLeft()
+      rot.elTurnDown()
 
     def orientToPoint(self, antLat, antLong, antElevation, satLat, satLong):
       #math equation
@@ -32,13 +44,23 @@ class Orientation:
       self.rot.elTurnUp() #until reaches correct angle
 
     def azLimitswitchHit(self):
-      print("here az 0")
-      self.rot.azReset()
-      print("here az")
-      Orientation.az = AZ_LIMIT_ORIENTATION
+      print("Called azLimitswitchHit()")
+      if not self.azOrientDone: # Don't run this if we aren't in orient init mode
+          self.azLimTriggers += 1
+          
+          if self.azLimTriggers > self.DEBOUNCE_VAL:
+              self.azOrientDone = True
+              self.azLimTriggers = 0
+              self.rot.azReset()
+              Orientation.az = AZ_LIMIT_ORIENTATION
 
     def elLimitswitchHit(self):
-      print("here el 0")
-      self.rot.elReset()
-      print("here el")
-      Orientation.el = EL_LIMIT_ORIENTATION
+      print("Called elLimitswitchHit()")
+      if not self.elOrientDone: # Don't run this if we aren't in orient init mode
+          self.elLimTriggers += 1
+          
+          if self.elLimTriggers > self.DEBOUNCE_VAL:
+              self.elOrientDone = True
+              self.elLimTriggers = 0
+              self.rot.elReset()
+              Orientation.el = EL_LIMIT_ORIENTATION
