87e68f47bb8fda08b81e4de7d1030eb7feb55f58
laural12 <laura.m.landon@gmail.com>
2023-02-06 08:14:58 -0700

    experimenting

--- flaskFiles/hello.py
+++ flaskFiles/hello.py
@@ -1,8 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/", methods=["GET", "POST"])
 def hello():
-    return 'Hello, World!'
+    if request.method == "POST":
+        if request.form.get("action1") == "VALUE1":
+            pass  # do something
+        elif request.form.get("action2") == "VALUE2":
+            pass  # do something else
+        else:
+            pass  # unknown
+    elif request.method == "GET":
+        return render_template("hello.html", form=form)
+
+    return render_template("hello.html")
--- installationSetup.py
+++ installationSetup.py
@@ -1,7 +1,23 @@
 # THIS SCRIPT TO BE RUN THE FIRST TIME YOU INSTALL AN ANTENNA
-# 
+#
 
+import pickle
+
+antData = {}
 
 # PROMPT USER FOR ANTENNA NUMBER, LAT, LONG, AND ORIENT CALIBRATION
+antData["antNum"] = input("What is the number of this antenna? ")
+antData["lat"] = input("What is the latitude of this antenna? ")
+antData["lon"] = input("What is the longitude of this antenna? ")
+antData["calAz"] = input(
+    "What is the azimuth calibration orientation of this antenna? "
+)
+antData["calEl"] = input(
+    "What is the elevation calibration orientation of this antenna? "
+)
 
-# WRITE TO FILE TITLED antXXConstants??
+with open("antConstants.pickle", "wb") as handle:
+    pickle.dump(antData, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+# with open('filename.pickle', 'rb') as handle:
+#     b = pickle.load(handle)
