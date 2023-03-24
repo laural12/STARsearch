ae4be707ca6bed131e07ba0fb79128635ac08ffb
dmartin28 <david.r.martin28@gmail.com>
2023-03-24 08:48:19 -0600

    Added autofind and orientation initialization to rotation

--- rotation.py
+++ rotation.py
@@ -35,13 +35,18 @@ class RotationBase:
 
     maxAngle = 360.0 #Unknown needs calibration
     maxVoltage = 1.8 #Assumed
+    azTolerance = 0.1
+    elTolerance = 0.1
     polDegreesPerVolt = maxAngle/maxVoltage
     VoltsPerADCVal = 1.8/(2**10 - 1) # about 0.00176V/ADC
 
     EL_LEFT_PWM = 21 # UP
     EL_RIGHT_PWM = 22 # DOWN
     AZ_LEFT_PWM = 28
-    AZ_RIGHT_PWM = 30       
+    AZ_RIGHT_PWM = 30
+    POL_LEFT_PWM = 19
+    POL_RIGHT_PWM = 18
+    POL_POTENTIOMETER = 0 #AIN0 ???????      
 
     # input pins
     AZ_ENC1 = 31
@@ -50,16 +55,14 @@ class RotationBase:
     # EL_ENC2 =
     LIMIT_ENC_AZ = 29
     LIMIT_ENC_EL = 24
-    POL_LEFT_PWM = 19
-    POL_RIGHT_PWM = 18
-    POL_POTENTIOMETER = 0 #AIN0 ???????
 
     def __init__(self, GUI_test=False):
         self.GUI_test = GUI_test
         self.azTicks = 0
         self.elTicks = 0
-        self.initialAz = 150.0
-        self.initialEl = 0.0
+        self.azOfAzLimitSwitch = 134.5
+        self.elOfElLimiSwitch = 12.85
+        self.azOfEllimitSwitchAz = 169.47
        
 
         if not self.GUI_test:
@@ -75,14 +78,75 @@ class RotationBase:
             wpi.pinMode(self.POL_RIGHT_PWM, self.OUTPUT)  # GPIO 18
             
             #Initializing Hall Sensors
-            self.azHall = AzimuthHallSensors(self.initialAz, self.AZ_ENC1)
-            self.elHall = ElevationHallSensors(self.initialEl, self.EL_ENC1)
+            self.azHall = AzimuthHallSensors(0, self.AZ_ENC1)
+            self.elHall = ElevationHallSensors(0, self.EL_ENC1)
 
 
     def initialize_orientation(self):
         print("Called function initialize_orientation()")
+
+        #Turn left until hitting limit switch
         self.azTurnLeft()
-        while ()
+        while (self.readLimAz == 0):
+            print("turning left until limit switch")
+
+        #Stop after hitting limit switch and reset az encoder
+        self.azReset()
+        self.azHall = AzimuthHallSensors(self.azOfAzLimitSwitch, self.AZ_ENC1)
+        
+        #Turn right until reaching elevation limit switch azimuth
+        self.azTurnRight()
+        while(self.getAzAngle < self.azOfEllimitSwitchAz):
+            print("turning right to el limit switch")
+        self.azReset()
+
+        #Turn down until hitting el limit switch
+        self.elTurnDown()
+        while(self.readLimEl == 0):
+            print("Turning down")
+        #Stop hall sensors and reinitialize limit switch
+        self.elReset()
+        self.elHall = ElevationHallSensors(self.elOfElLimiSwitch, self.EL_ENC1)
+    
+    def autoFind(self, azDesired, elDesired):
+        #Calculate Errors
+        azError = float(self.getAzAngle()) - azDesired
+        elError = float(self.getElAngle()) - elDesired
+
+        try:
+            #Control Loop
+            while (abs(azError) > self.azTolerance or abs(elError) > self.elTolerance):
+                print("Angles")
+                print(self.getAzAngle())
+                print(self.getElAngle())
+                if (self.getAzAngle() > azDesired + self.azTolerance):
+                    #turn counterclockwise
+                    self.azTurnLeft()
+                elif (self.getAzAngle() < azDesired-self.azTolerance):
+                    #turn clockwise
+                    self.azTurnRight()
+                else:
+                    self.azReset()
+
+                if (self.getElAngle() < elDesired - self.elTolerance):
+                    #extend actuator / Lower Elevation
+                    self.elTurnUp()
+                elif (self.getElAngle() > elDesired + self.elTolerance):
+                    #retract actuator / Raise Elevation
+                    self.elTurnDown()
+                else:
+                    self.elReset()
+
+                #recalculate errors
+                azError = float(self.getAzAngle()) - azDesired
+                elError = float(self.getElAngle()) - elDesired
+            
+            self.azReset()
+            self.elReset()
+        
+        except KeyboardInterrupt:
+            self.azReset()
+            self.elReset()
 
     def write(self, pinNum, writeVal):
         if not self.GUI_test:
