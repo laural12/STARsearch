5ac78b12b0fc536c9c18e49ea496dd4b1afc763d
laural12 <laura.m.landon@gmail.com>
2023-03-24 08:15:44 -0600

    add higher level functions

--- manualRotateGUI.py
+++ manualRotateGUI.py
@@ -18,7 +18,8 @@ import PySimpleGUI as sg
 
 from rotation import Rotation
 from orientation import Orientation
-#from fieldFox import FieldFox
+
+# from fieldFox import FieldFox
 print("imported orientation")
 
 sg.theme("DarkBlue12")
@@ -31,34 +32,89 @@ def make_window():
     aziTip = "Manually input desired azimuth"
     eleTip = "Manually input desired elevation"
 
-    leftCol = [
-        [sg.Text("Azimuth")],
-        [sg.Button("Az turn left")],
-        [sg.Button("Az turn right")],
-        [sg.Button("Reset Az")],
-        [sg.Text("Elevation")],
-        [sg.Button("El turn down")],
-        [sg.Button("El turn up")],
-        [sg.Button("Reset El")],
-    ]
-
-    midCol = [
+    outputs = [
         [sg.Text(key="Az limit switch")],
         [sg.Text(key="Az Angle")],
         [sg.Text(key="El Angle")],
         [sg.Text(key="FF input")],
+        [sg.Text(key="Pol value")],
+    ]
+    outputsFr = sg.Frame("Readings", outputs)
+
+    polarization = [
+        [sg.Button("Pol right")],
+        [sg.Button("Pol left")],
+        [sg.Button("Stop pol", button_color=('green'))],
+        [sg.Text("Enter desired pol angle")],
+        [
+            sg.Input(
+                size=(10, 1),
+                key="pol_angle",
+            )
+        ],
+        [sg.Button("Auto Pol")],
+    ]
+    polarizationFr = sg.Frame("Polarization", polarization)
+
+    autoFind = [
+        [sg.Text("Enter desired azimuth")],
+        [
+            sg.Input(
+                size=(10, 1),
+                key="autofind_az",
+            )
+        ],
+        [sg.Text("Enter desired elevation")],
+        [
+            sg.Input(
+                size=(10, 1),
+                key="autofind_el",
+            )
+        ],
+        [sg.Button("Auto Find")],
+    ]
+    autoFindFr = sg.Frame("Auto Find", autoFind)
+
+    autoPeak = [
+        [sg.Text("Enter frequency")],
+        [
+            sg.Input(
+                size=(10, 1),
+                key="autopeak_freq",
+            )
+        ],
+        [sg.Button("Auto Peak")],
+    ]
+    autoPeakFr = sg.Frame("Auto Peak", autoPeak)
+
+    orientCal = [
+        [sg.Button("Orient Init")],
+    ]
+    orientCalFr = sg.Frame("Calibrate Orientation", orientCal)
+
+    leftCol = [
+        [sg.Text("Azimuth")],
+        [sg.Button("Az turn left")],
+        [sg.Button("Az turn right")],
+        [sg.Button("Stop Az", button_color=("green"))],
+        [sg.Text("Elevation")],
+        [sg.Button("El turn down")],
+        [sg.Button("El turn up")],
+        [sg.Button("Stop El", button_color=("green"))],
+        [sg.Text("Stop all")],
+        [sg.Button("Stop all", button_color=("green"))],
+    ]
+
+    midCol = [
+        [outputsFr],
+        [polarizationFr],
     ]
 
     rightCol = [
-        [sg.Text("Pin control")],
-        [sg.Button("GPIO 24")],
-        [sg.Button("GPIO 21")],
-        [sg.Button("GPIO 22")],
-        [sg.Button("GPIO 19")],
-        [sg.Button("GPIO 28")],
-        [sg.Button("GPIO 30")],
-        [sg.Button("Reset all")],
-        [sg.Button("Cancel")],
+        [autoFindFr],
+        [orientCalFr],
+        [autoPeakFr],
+        [sg.Button("Close GUI")],
     ]
 
     layout = [
@@ -85,10 +141,10 @@ window = sg.Window("Manual antenna control", make_window(), finalize=True)
 
 
 # Instantiate rotate class
-myRotate = Rotation(GUI_test=False)
+myRotate = Rotation(GUI_test=True)
 orient = Orientation()
-orient.orientation_init()
-#mySignal = FieldFox()
+# orient.orientation_init()
+# mySignal = FieldFox()
 
 # CONSIDER CREATING A FUNCTION DICT???
 
@@ -96,8 +152,8 @@ orient.orientation_init()
 while True:
     event, values = window.read(timeout=10)
     if (
-        event == sg.WIN_CLOSED or event == "Cancel"
-    ):  # if user closes window or clicks cancel
+        event == sg.WIN_CLOSED or event == "Close GUI"
+    ):  # if user closes window or clicks Close GUI
         break
     elif event == "Az turn left":
         print("Az turn left")
@@ -111,38 +167,46 @@ while True:
     elif event == "El turn up":
         print("El turn up")
         myRotate.elTurnUp()
-    elif event == "Reset Az":
-        print("Reset Az")
+    elif event == "Stop Az":
+        print("Stop Az")
         myRotate.azReset()
-    elif event == "Reset El":
-        print("Reset El")
+    elif event == "Stop El":
+        print("Stop El")
         myRotate.elReset()
-    elif event == "Reset all":
-        print("Reset all")
+    elif event == "Stop all":
+        print("Stop all")
         myRotate.elReset()
         myRotate.azReset()
-    elif event == "GPIO 24":
-        print("GPIO 24 to high")
-        myRotate.write(24, 1)
-    elif event == "GPIO 21":
-        print("GPIO 21 to high")
-        myRotate.write(21, 1)
-    elif event == "GPIO 22":
-        print("GPIO 22 to high")
-        myRotate.write(22, 1)
-    elif event == "GPIO 19":
-        print("GPIO 19 to high")
-        myRotate.write(19, 1)
-    elif event == "GPIO 28":
-        print("GPIO 28 to high")
-        myRotate.write(28, 1)
-    elif event == "GPIO 30":
-        print("GPIO 30 to high")
-        myRotate.write(30, 1)
+    elif event == "Auto Find":
+        print("Auto Find")
+        print(f"az: {values['autofind_az']}")
+        print(f"el: {values['autofind_el']}")
+        # myRotate.autoFindTest()
+    elif event == "Orient Init":
+        print("Orient Init")
+        # orient.orientation_init()
+    elif event == "Pol right":
+        print("Pol right")
+        # myRotate.polRight()
+    elif event == "Pol left":
+        print("Pol left")
+        # myRotate.polLeft()
+    elif event == "Stop pol":
+        print("Stop pol")
+        # myRotate.stopPol()
+    elif event == "Auto Pol":
+        print("Auto Pol")
+        print(values["pol_angle"])
+        # myRotate.autoPol(values["pol_angle"])
+    elif event == "Auto Peak":
+        print("Auto Peak")
+        print(f"Freq: {values['autopeak_freq']}")
+        # myRotate.autoPeak(values['autopeak_freq'])
 
     window["Az limit switch"].update(f"Az lim: {myRotate.readLimAz()}")
     window["El Angle"].update(f"El Angle: {myRotate.getElAngle()}")
     window["Az Angle"].update(f"Az Angle: {myRotate.getAzAngle()}")
-    #window["Az ticks"].update(f"Az ticks: {myRotate.getAzTicks()}")
-    #window["El ticks"].update(f"El ticks: {myRotate.getElTicks()}")
-    #window["FF input"].update(f"FF input: {mySignal.readSig()}")
+    window["Pol value"].update(f"Pol value: {myRotate.getPolAngle()}")
+    # window["Az ticks"].update(f"Az ticks: {myRotate.getAzTicks()}")
+    # window["El ticks"].update(f"El ticks: {myRotate.getElTicks()}")
+    # window["FF input"].update(f"Channel power: {mySignal.readSig()}")
